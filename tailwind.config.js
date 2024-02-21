0d515c8d50a0c8ca3924fa740dc1274f88bc5f6e
tavolafourcade <tavolafourcade@gmail.com>
2024-02-20 20:35:12 -0500

    [ ZAIC-14 ] UPDATE tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}'
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [],
+  theme  : {
+    extend: {
+      colors: {
+        light    : '#F5F4EE',
+        primary  : '#B0070D',
+        secondary: '#326259'
+      },
+      fontFamily: {
+        gantari: [ 'var(--font-gantari)' ],
+        onest  : [ 'var(--font-onest)' ]
+      }
+    }
+  }
 }
