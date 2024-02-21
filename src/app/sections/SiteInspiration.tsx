d56950a3e97362dac0c74b842c4bf92ae510569c
tavolafourcade <tavolafourcade@gmail.com>
2024-02-20 21:29:31 -0500

    [ ZAIC-14 ] ADD SiteInspiration section

---
+++ src/app/sections/SiteInspiration.tsx
@@ -0,0 +1,15 @@
+import { useTranslations } from 'next-intl'
+
+const SiteInspiration = () => {
+  const t = useTranslations()
+
+  return (
+    <section className='py-[168px] px-[156px]' id='inspiration'>
+      <div className='max-w-[648px]'>
+        <p className='text-5xl leading-15 text-alt font-bold'>{t('phrase_1')}</p>
+        <p className='text-5xl leading-15 text-secondary font-bold'>{t('phrase_2')}</p>
+      </div>
+    </section>
+  )
+}
+export default SiteInspiration
