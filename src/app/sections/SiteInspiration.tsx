9fefb0e821049d41480cbcb76adc8926cc79968a
tavolafourcade <tavolafourcade@gmail.com>
2024-02-20 22:28:09 -0500

    [ ZAIC-14 ] UPDATE Responsiveness for SiteInspiration

--- src/app/sections/SiteInspiration.tsx
+++ src/app/sections/SiteInspiration.tsx
@@ -4,9 +4,9 @@ const SiteInspiration = () => {
   const t = useTranslations()
 
   return (
-    <section className='py-[168px] px-[156px]' id='inspiration'>
+    <section className='py-[168px] px-4 lg:px-[156px]' id='inspiration'>
       <div className='max-w-[648px]'>
-        <p className='text-5xl leading-15 text-alt font-bold'>{t('phrase_1')}</p>
+        <p className='text-5xl leading-15 text-accent font-bold'>{t('phrase_1')}</p>
         <p className='text-5xl leading-15 text-secondary font-bold'>{t('phrase_2')}</p>
       </div>
     </section>
